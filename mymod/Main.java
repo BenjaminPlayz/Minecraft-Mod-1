bd35ff4cb1f1b2b9d591464114fb234c3d743227
Benjamin Brown <bdb0918@gmail.com>
2015-02-23 21:00:36 -0500

    Organized code

--- mymod/Main.java
+++ mymod/Main.java
@@ -219,22 +219,51 @@ public class Main {
  * LOAD SECTION 
  * *********************************************************** */ 
 
- //  LOAD THE SWORD
+ //  SWORD AREA
         MySword_1 = new MySword(2021, MyToolMaterial, "MySword_1");
         GameRegistry.registerItem(MySword_1, "MySword_1");
         LanguageRegistry.addName(MySword_1, "Magma Blade"); 
 
-//  LOAD THE PICKAXE
+
+        MySword_2 = new MySword2(2024, MyToolMaterialCrimsonateSword, "MySword_2");
+        GameRegistry.registerItem(MySword_2, "MySword_2");
+        LanguageRegistry.addName(MySword_2, "Crimsonate Sword");  
+
+
+        MySword_3 = new MySword(2093, MyToolMaterialFleshSword, "MySword_3");
+        GameRegistry.registerItem(MySword_3, "MySword_3");
+        LanguageRegistry.addName(MySword_3, "Flesh Sword");     
+
+
+        MySword_4 = new MySword4(2102, MyToolMaterialUthoria, "MySword_4");
+        GameRegistry.registerItem(MySword_4, "MySword_4");
+        LanguageRegistry.addName(MySword_4, "Uthoria");
+
+
+        MySword_5 = new MySword5(2103, MyToolMaterialCreative, "MySword_5");
+        GameRegistry.registerItem(MySword_5, "MySword_5");
+        LanguageRegistry.addName(MySword_5, "Insta Killer");
+
+
+
+
+// PICKAXE AREA
         MyPickaxe_1 = new MyPickaxe(2022, MyToolMaterial, "MyPickaxe_1");
         GameRegistry.registerItem(MyPickaxe_1, "MyPickaxe_1");
         LanguageRegistry.addName(MyPickaxe_1, "Ice Pickaxe"); 
 	
-	//  LOAD THE PICKAXE
+
         MyPickaxe_2 = new MyPickaxe(2023, MyToolMaterialCrimsonatePickaxe, "MyPickaxe_2");
         GameRegistry.registerItem(MyPickaxe_2, "MyPickaxe_2");
         LanguageRegistry.addName(MyPickaxe_2, "Crimsonate Pickaxe"); 
 	
-	//  LOAD THE BLOCK 
+	
+        MyPickaxe_3 = new MyPickaxe(2091, MyToolMaterialFleshPick, "MyPickaxe_3");
+        GameRegistry.registerItem(MyPickaxe_3, "MyPickaxe_3");
+        LanguageRegistry.addName(MyPickaxe_3, "Flesh Pickaxe"); 
+	
+	
+	//  BLOCK AREA
                                                                     /** Hardness is how long it takes to mine block, Resistance is how strong the block is to Explosions */     	
         MyBlock_1 = new MyBlock(250, Material.rock, "MyBlock_1").setLightValue(0.7586F).setHardness(10.5F).setResistance(14.5F).setStepSound(Block.soundStoneFootstep);
         GameRegistry.registerBlock(MyBlock_1, "MyBlock_1");
@@ -246,14 +275,13 @@ public class Main {
         LanguageRegistry.addName(MyBlock_2, "Crimsified Grass"); 
 		MinecraftForge.setBlockHarvestLevel(MyBlock_2, "pickaxe", 1);
 
-
- //  LOAD THE BLOCK 
+ 
         MyBlock_3 = new MyBlock(2080, Material.grass, "MyBlock_3");
         GameRegistry.registerBlock(MyBlock_3, "MyBlock_3");
         LanguageRegistry.addName(MyBlock_3, "Crimsified Stone"); 
 		MinecraftForge.setBlockHarvestLevel(MyBlock_3, "pickaxe", 1);
 
- //  LOAD THE BLOCK 
+ 
         MyBlock_4 = new MyBlock(252, Material.grass, "MyBlock_4").setHardness(0.6F).setStepSound(Block.soundGrassFootstep);
         GameRegistry.registerBlock(MyBlock_4, "MyBlock_4");
         LanguageRegistry.addName(MyBlock_4, "Corrupt Grass"); 
@@ -261,50 +289,63 @@ public class Main {
 
 
 
-	//  LOAD THE ITEM
+	// ITEM AREA
         MyItem_1 = new MyItem(2030, "MyItem_1").setCreativeTab(CreativeTabs.tabMaterials).setMaxStackSize(64);
         GameRegistry.registerItem(MyItem_1, "MyItem_1");
         LanguageRegistry.addName(MyItem_1, "Crimsonate Ingot");  
     
     
-    //  LOAD THE SWORD
-        MySword_2 = new MySword2(2024, MyToolMaterialCrimsonateSword, "MySword_2");
-        GameRegistry.registerItem(MySword_2, "MySword_2");
-        LanguageRegistry.addName(MySword_2, "Crimsonate Sword");  
+        MyItem_2 = new MyItem(2031, "MyItem_2").setCreativeTab(CreativeTabs.tabMaterials).setMaxStackSize(64);
+        GameRegistry.registerItem(MyItem_2, "MyItem_2");
+        LanguageRegistry.addName(MyItem_2, "Crimsonate Stick");  
     
     
-     //  LOAD THE AXE
+        MyItem_3 = new MyItem(2090, "MyItem_3").setCreativeTab(CreativeTabs.tabMaterials);
+        GameRegistry.registerItem(MyItem_3, "MyItem_3");
+        LanguageRegistry.addName(MyItem_3, "Flesh Ingot");  
+	
+    
+        MyItem_4 = new MyItem(2092, "MyItem_4").setCreativeTab(CreativeTabs.tabMaterials);
+        GameRegistry.registerItem(MyItem_4, "MyItem_4");
+        LanguageRegistry.addName(MyItem_4, "Flesh Stick");  
+    
+    
+        MyItem_5 = new MyItem(2098, "MyItem_5").setCreativeTab(CreativeTabs.tabMaterials).setMaxStackSize(64);
+        GameRegistry.registerItem(MyItem_5, "MyItem_5");
+        LanguageRegistry.addName(MyItem_5, "Crimsonate Shard");  
+	
+	
+        MyItem_6 = new MyItem(2099, "MyItem_6").setCreativeTab(CreativeTabs.tabMaterials).setMaxStackSize(64);
+        GameRegistry.registerItem(MyItem_6, "MyItem_6");
+        LanguageRegistry.addName(MyItem_6, "Crimsonate Chunk");  
+	
+   
+        MyProjectile_1 = new  MyProjectile(2101, " MyProjectile_1").setCreativeTab(CreativeTabs.tabMaterials);
+        GameRegistry.registerItem( MyProjectile_1, " MyProjectile_1");
+        LanguageRegistry.addName( MyProjectile_1, "The Boomstick");  
+        
+    
+    
+    
+     //  AXE AREA
         MyAxe_1 = new MyAxe(2100, MyToolMaterialCrimsonatePickaxe, "MyAxe_1");
         GameRegistry.registerItem(MyAxe_1, "MyAxe_1");
         LanguageRegistry.addName(MyAxe_1, "Crimsonate Axe");  
     
     
      
-    //  LOAD THE SWORD
-        MySword_4 = new MySword4(2102, MyToolMaterialUthoria, "MySword_4");
-        GameRegistry.registerItem(MySword_4, "MySword_4");
-        LanguageRegistry.addName(MySword_4, "Uthoria");
-    
-    
-    //  LOAD THE SWORD
-        MySword_5 = new MySword5(2103, MyToolMaterialCreative, "MySword_5");
-        GameRegistry.registerItem(MySword_5, "MySword_5");
-        LanguageRegistry.addName(MySword_5, "Insta Killer");
     
     
     
-     //  LOAD THE ITEM
-        MyItem_2 = new MyItem(2031, "MyItem_2").setCreativeTab(CreativeTabs.tabMaterials).setMaxStackSize(64);
-        GameRegistry.registerItem(MyItem_2, "MyItem_2");
-        LanguageRegistry.addName(MyItem_2, "Crimsonate Stick");  
     
-     //  LOAD THE FOOD
+    
+    
+     // FOOD AREA
                           /** First Vaule is HealPoints, Then Saturation Value, Then the last is True or False and it is saying IsWolfsFavoriteFood. */     
         MyFood_1 = new MyFood(2040, 4, 2.5F, true, "MyFood_1").setAlwaysEdible().setPotionEffect(Potion.moveSpeed.id, 45, 8, 0.1F).setCreativeTab(CreativeTabs.tabFood).setMaxStackSize(32);
         GameRegistry.registerItem(MyFood_1, "MyFood_1");
         LanguageRegistry.addName(MyFood_1, "Marshmallow"); 
-    
-         //  LOAD THE FOOD
+ 
                           /** First Vaule is HealPoints, Then Saturation Value, Then the last is True or False and it is saying IsWolfsFavoriteFood. */     
         MyFood_2 = new MyFood(2104, 6, 4.5F, true, "MyFood_2").setPotionEffect(Potion.resistance.id, 45, 8, 0.1F).setCreativeTab(CreativeTabs.tabFood).setMaxStackSize(64);
         GameRegistry.registerItem(MyFood_2, "MyFood_2");
@@ -312,37 +353,72 @@ public class Main {
     
     
     
-    //  LOAD HELMET 
+    //  ARMOR AREA 
         MyHelmet_1 = new MyArmor(2060, MyArmorMaterial_1, 0, 0, "myarmor").setCreativeTab(CreativeTabs.tabCombat);
         GameRegistry.registerItem(MyHelmet_1, "MyHelmet_1");
         LanguageRegistry.addName(MyHelmet_1, "Crimsonate Helmet");      
     
-//  LOAD CHESTPLATE
+    
         MyChest_1 = new MyArmor(2061, MyArmorMaterial_1, 0, 1, "myarmor").setCreativeTab(CreativeTabs.tabCombat);
         GameRegistry.registerItem(MyChest_1, "MyChest_1");
         LanguageRegistry.addName(MyChest_1, "Crimsonate Chestplate");
 
-//  LOAD LEGGINGS    
+
         MyLeggings_1 = new MyArmor(2062, MyArmorMaterial_1, 0, 2, "myarmor").setCreativeTab(CreativeTabs.tabCombat);
         GameRegistry.registerItem(MyLeggings_1, "MyLeggings_1");
         LanguageRegistry.addName(MyLeggings_1, "Crimsonate Leggings");
 
-//  LOAD BOOTS   
+
         MyBoots_1 = new MyArmor(2063, MyArmorMaterial_1, 0, 3, "myarmor").setCreativeTab(CreativeTabs.tabCombat);
         GameRegistry.registerItem(MyBoots_1, "MyBoots_1");
         LanguageRegistry.addName(MyBoots_1, "Crimsonate Feet");
     
-     //  LOAD BIOME
+        MyHelmet_2 = new MyFleshArmor(2094, MyArmorMaterial_2, 0, 0, "myflesharmor");
+        GameRegistry.registerItem(MyHelmet_2, "MyHelmet_2");
+        LanguageRegistry.addName(MyHelmet_2, "Flesh Helmet");      
+
+        MyChest_2 = new MyFleshArmor(2095, MyArmorMaterial_2, 0, 1, "myflesharmor");
+        GameRegistry.registerItem(MyChest_2, "MyChest_2");
+        LanguageRegistry.addName(MyChest_2, "Flesh Chestplate");
+
+        MyLeggings_2 = new MyFleshArmor(2096, MyArmorMaterial_2, 0, 2, "myflesharmor");
+        GameRegistry.registerItem(MyLeggings_2, "MyLeggings_2");
+        LanguageRegistry.addName(MyLeggings_2, "Flesh Legs");
+
+        MyBoots_2 = new MyFleshArmor(2097, MyArmorMaterial_2, 0, 3, "myflesharmor");
+        GameRegistry.registerItem(MyBoots_2, "MyBoots_2");
+        LanguageRegistry.addName(MyBoots_2, "Flesh Greaves");
+        
+        MyHelmet_3 = new MyLapisArmor(2105, MyArmorMaterial_3, 0, 0, "mylapisarmor");
+        GameRegistry.registerItem(MyHelmet_3, "MyHelmet_3");
+        LanguageRegistry.addName(MyHelmet_3, "Lapis Helmet");      
+    
+
+        MyChest_3 = new MyLapisArmor(2106, MyArmorMaterial_3, 0, 1, "mylapisarmor");
+        GameRegistry.registerItem(MyChest_3, "MyChest_3");
+        LanguageRegistry.addName(MyChest_3, "Lapis Chestplate");
+
+
+        MyLeggings_3 = new MyLapisArmor(2107, MyArmorMaterial_3, 0, 2, "mylapisarmor");
+        GameRegistry.registerItem(MyLeggings_3, "MyLeggings_3");
+        LanguageRegistry.addName(MyLeggings_3, "Lapis Legs");
+
+
+        MyBoots_3 = new MyLapisArmor(2108, MyArmorMaterial_3, 0, 3, "mylapisarmor");
+        GameRegistry.registerItem(MyBoots_3, "MyBoots_3");
+        LanguageRegistry.addName(MyBoots_3, "Lapis Boots");
+    
+    
+
+    
+     //  BIOMES AREA
         MyBiome_1 = new MyBiome(30);
         GameRegistry.addBiome(MyBiome_1);
-        
-	
-	  //  LOAD BIOME
+       
         MyCorruptionBiome_1 = new MyCorruptionBiome(31);
         GameRegistry.addBiome(MyCorruptionBiome_1);
 
-   
-	 //  REGISTER YOUR ENTITY
+        // ENTITY AREA MOBS
         EntityRegistry.registerGlobalEntityID(MyEntityZombie.class, "Face Gobler", EntityRegistry.findGlobalUniqueEntityId());
         EntityRegistry.addSpawn(MyEntityZombie.class, 50, 1, 5, EnumCreatureType.monster, BiomeGenBase.desert); 
         EntityRegistry.addSpawn(MyEntityZombie.class, 50, 1, 5, EnumCreatureType.monster, MyBiome_1);     
@@ -350,102 +426,13 @@ public class Main {
         RenderingRegistry.registerEntityRenderingHandler(MyEntityZombie.class, new MyRenderZombie());
         ModLoader.addLocalization("entity.Face Gobler.name", "Face Gobler");
 	
-   
-	//  LOAD THE ITEM
-        MyItem_3 = new MyItem(2090, "MyItem_3").setCreativeTab(CreativeTabs.tabMaterials);
-        GameRegistry.registerItem(MyItem_3, "MyItem_3");
-        LanguageRegistry.addName(MyItem_3, "Flesh Ingot");  
-	
-	
-	 //  LOAD THE PICKAXE
-        MyPickaxe_3 = new MyPickaxe(2091, MyToolMaterialFleshPick, "MyPickaxe_3");
-        GameRegistry.registerItem(MyPickaxe_3, "MyPickaxe_3");
-        LanguageRegistry.addName(MyPickaxe_3, "Flesh Pickaxe");  
-	
-  //  LOAD THE ITEM
-        MyItem_4 = new MyItem(2092, "MyItem_4").setCreativeTab(CreativeTabs.tabMaterials);
-        GameRegistry.registerItem(MyItem_4, "MyItem_4");
-        LanguageRegistry.addName(MyItem_4, "Flesh Stick");  
-    
-	
-	//  LOAD THE BOOMSTICK ITEM
-        MyProjectile_1 = new  MyProjectile(2101, " MyProjectile_1").setCreativeTab(CreativeTabs.tabMaterials);
-        GameRegistry.registerItem( MyProjectile_1, " MyProjectile_1");
-        LanguageRegistry.addName( MyProjectile_1, "The Boomstick");  
-        
-        
-         //   REGISTER THE PROJECTILE ENTITY
+  
          EntityRegistry.registerGlobalEntityID(MyEntityProjectile.class, "MyProjectile_1", 1);
          EntityRegistry.registerModEntity(MyEntityProjectile.class, "MyProjectile_1", 1, this, 256, 1, true);
          LanguageRegistry.instance().addStringLocalization("entity.modid.MyProjectile_1.name", "My Awesome Projectile");
          RenderingRegistry.registerEntityRenderingHandler(MyEntityProjectile.class, new MyRenderProjectile(MyProjectile_1));
          
-        
-	
-	 //  LOAD THE SWORD
-        MySword_3 = new MySword(2093, MyToolMaterialFleshSword, "MySword_3");
-        GameRegistry.registerItem(MySword_3, "MySword_3");
-        LanguageRegistry.addName(MySword_3, "Flesh Sword");     
-
-    
-     //  LOAD HELMET 
-        MyHelmet_2 = new MyFleshArmor(2094, MyArmorMaterial_2, 0, 0, "myflesharmor");
-        GameRegistry.registerItem(MyHelmet_2, "MyHelmet_2");
-        LanguageRegistry.addName(MyHelmet_2, "Flesh Helmet");      
-    
-//  LOAD CHESTPLATE
-        MyChest_2 = new MyFleshArmor(2095, MyArmorMaterial_2, 0, 1, "myflesharmor");
-        GameRegistry.registerItem(MyChest_2, "MyChest_2");
-        LanguageRegistry.addName(MyChest_2, "Flesh Chestplate");
-
-//  LOAD LEGGINGS    
-        MyLeggings_2 = new MyFleshArmor(2096, MyArmorMaterial_2, 0, 2, "myflesharmor");
-        GameRegistry.registerItem(MyLeggings_2, "MyLeggings_2");
-        LanguageRegistry.addName(MyLeggings_2, "Flesh Legs");
-
-//  LOAD BOOTS   
-        MyBoots_2 = new MyFleshArmor(2097, MyArmorMaterial_2, 0, 3, "myflesharmor");
-        GameRegistry.registerItem(MyBoots_2, "MyBoots_2");
-        LanguageRegistry.addName(MyBoots_2, "Flesh Greaves");
-    
-    
-        //  LOAD HELMET 
-        MyHelmet_3 = new MyLapisArmor(2105, MyArmorMaterial_3, 0, 0, "mylapisarmor");
-        GameRegistry.registerItem(MyHelmet_3, "MyHelmet_3");
-        LanguageRegistry.addName(MyHelmet_3, "Lapis Helmet");      
-    
-//  LOAD CHESTPLATE
-        MyChest_3 = new MyLapisArmor(2106, MyArmorMaterial_3, 0, 1, "mylapisarmor");
-        GameRegistry.registerItem(MyChest_3, "MyChest_3");
-        LanguageRegistry.addName(MyChest_3, "Lapis Chestplate");
-
-//  LOAD LEGGINGS    
-        MyLeggings_3 = new MyLapisArmor(2107, MyArmorMaterial_3, 0, 2, "mylapisarmor");
-        GameRegistry.registerItem(MyLeggings_3, "MyLeggings_3");
-        LanguageRegistry.addName(MyLeggings_3, "Lapis Legs");
-
-//  LOAD BOOTS   
-        MyBoots_3 = new MyLapisArmor(2108, MyArmorMaterial_3, 0, 3, "mylapisarmor");
-        GameRegistry.registerItem(MyBoots_3, "MyBoots_3");
-        LanguageRegistry.addName(MyBoots_3, "Lapis Boots");
-    
-    
-    
-    
-	//  LOAD THE ITEM
-        MyItem_5 = new MyItem(2098, "MyItem_5").setCreativeTab(CreativeTabs.tabMaterials).setMaxStackSize(64);
-        GameRegistry.registerItem(MyItem_5, "MyItem_5");
-        LanguageRegistry.addName(MyItem_5, "Crimsonate Shard");  
-	
-	
-		//  LOAD THE ITEM
-        MyItem_6 = new MyItem(2099, "MyItem_6").setCreativeTab(CreativeTabs.tabMaterials).setMaxStackSize(64);
-        GameRegistry.registerItem(MyItem_6, "MyItem_6");
-        LanguageRegistry.addName(MyItem_6, "Crimsonate Chunk");  
-	
-	
-	
-	 //  REGISTER YOUR ENTITY
+       
         EntityRegistry.registerGlobalEntityID(MyEntityWraith.class, "Corrupt Wraith", EntityRegistry.findGlobalUniqueEntityId());  
         EntityRegistry.addSpawn(MyEntityWraith.class, 1, 0, 0, EnumCreatureType.monster, MyCorruptionBiome_1);  
         registerEntityEgg(MyEntityWraith.class, (new Color(255, 255, 255)).getRGB(), (new Color(0, 0, 0)).getRGB());
@@ -453,19 +440,18 @@ public class Main {
         ModLoader.addLocalization("entity.Corrupt Wraith.name", "Corrupt Wraith");
 	
 	
-		 //  REGISTER YOUR ENTITY
         EntityRegistry.registerGlobalEntityID(MyEntityWraithlord.class, "Wraith Lord", EntityRegistry.findGlobalUniqueEntityId());  
         EntityRegistry.addSpawn(MyEntityWraithlord.class, 1, 1, 1, EnumCreatureType.monster, MyBiome_1);  
         registerEntityEgg(MyEntityWraithlord.class, (new Color(255, 255, 255)).getRGB(), (new Color(0, 0, 0)).getRGB());
         RenderingRegistry.registerEntityRenderingHandler(MyEntityWraithlord.class, new MyRenderWraithlord(new MyModelWraithlord(), 0.3F));
         ModLoader.addLocalization("entity.Wraith Lord.name", "Wraith Lord");
-	
+
 	
 	
 	
         
 	
-	//	LOAD THE ACHIEVEMENTS
+	// ACHIEVEMENT AREA
 		MyAchievement_1 = new Achievement(2001, "MyAchievement_1", -1, -1, MyItem_1, AchievementList.openInventory).registerAchievement();
 		MyAchievement_2 = new Achievement(2002, "MyAchievement_2", -2, -1, MyItem_4, AchievementList.openInventory).registerAchievement();
 		//	(id, "NameOfAchievement", x, y coordinates on Achievement map, icon, Required Achievement to unlock)
